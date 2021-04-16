713a1e92a8ccd8fdb079ddfea0e8f325c121f1df
unknown <jmls9627@gmail.com>
2021-04-16 10:11:11 -0400

    2.2 Debugging

--- src/Grade_Book/Program.cs
+++ src/Grade_Book/Program.cs
@@ -5,8 +5,11 @@ namespace Grade_Book
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+           if(args.Length>0) 
+            Console.WriteLine($"Hello, {args[0]}!");
+            else 
+            Console.WriteLine("Hello!!!!");
         }
     }
 }
