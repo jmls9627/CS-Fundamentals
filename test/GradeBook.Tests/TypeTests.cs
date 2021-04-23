d3cc1fcb7e7387a99396bee77ee130061dba148a
unknown <jmls9627@gmail.com>
2021-04-23 10:21:31 -0400

    6.3 Returning Obj Reference

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -7,6 +7,21 @@ namespace GradeBook.Tests
     public class TypeTests
     {
          [Fact]
+        public void CSharpIsPassByValue()
+        {
+          var book1= GetBook("Book 1");
+          GetBookSetName(book1,"New Name");
+          
+          Assert.Equal( "New Name",book1.name);
+        }
+
+        private void GetBookSetName(Book book, string Name)
+        {
+           book=new Book(Name);
+          
+        }
+
+         [Fact]
         public void CanSetNameFromReference()
         {
           var book1= GetBook("Book 1");
