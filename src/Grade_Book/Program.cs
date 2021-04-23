068138018b714f4ff8d1923ef6c627860c80b4b0
unknown <jmls9627@gmail.com>
2021-04-23 17:40:17 -0400

    7.2 Taking User input from the Console

--- src/Grade_Book/Program.cs
+++ src/Grade_Book/Program.cs
@@ -8,17 +8,31 @@ namespace Grade_Book
         static void Main(string[] args)
         {   
            var book = new Book("Pepe's Book");
-           book.AddGrade(89.1); 
-           book.AddGrade(34.4);       
-           book.AddGrade(12.3);
-           book.AddGrade(33.54);
+           var done=false;
 
+           
+           while(!done)
+           {
+               Console.WriteLine("Enter a Grade or Press 'q' to exit");
+               var input=Console.ReadLine();
+                            
+               if (input=="q")
+               {
+                   done=true;
+               }
+               else
+               {
+                 var grade=double.Parse(input);
+                 book.AddGrade(grade);
+               }      
+           }
+     
          var Stats = book.GetStats();
 
            Console.WriteLine($"the average grade is: {Stats.Average:N1}");
            Console.WriteLine($"the lowest grade is: {Stats.Low:N1} ");
            Console.WriteLine($"the hihgest grade is: {Stats.High:N1}");
-           Console.WriteLine($"the hihgest grade is: {Stats.Letter}");
+           Console.WriteLine($"the Letter grade is: {Stats.Letter}");
         }
     }
 }
